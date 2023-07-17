f0f73a383cbd82654c19e0d2509e61eff27af16f
akinlope <akinrimisitt@gmail.com>
2023-07-17 17:18:38 +0100

    [test push]

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="fixed z-50 bottom-8 right-3 md:right-12 flex flex-col items-center justify-center">
-            <p>Hello world</p>
+            
         </div>
     );
 }
